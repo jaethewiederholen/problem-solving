391ec421d0f81a55e766f3cede19c7e3aca81940
jennwiederholen <wm-il000530@WM-IL000530ui-MacBookAir.local>
2024-08-09 19:56:55 +0900

    Feat: day2

--- playground/main.go
+++ playground/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	
-	park := []string{"SOO","OXX","OOO"}
+	park := []string{"SOO","OOO","OOO"}
 	routes := []string {"E 2","S 2","W 1"}
 	fmt.Println(solution(park, routes))
 }
@@ -22,44 +22,73 @@ func solution(park []string, routes []string) []int {
         row := strings.Split(r, "")
         parkTd = append(parkTd, row)
     }
-	fmt.Println(parkTd)
+	// x 가 세로, y 가 가로
 	// find start
-	for y, r := range parkTd {
-		for x, c := range r {
+	for x, r := range parkTd {
+		for y, c := range r {
 			if c == "S" {
 				location = []int{x,y}
 			}
 		}
 	}
+	fmt.Println("start : ", location)
 
-	for _, r := range routes {
+
+	for _, r := range routes { 
+		//var newX, newY int
+		location[0], location[1] = move(location[0], location[1], r, parkTd)
 		fmt.Println(location)
-		var newX, newY int
-		newX, newY = move(location[0], location[1], r)
-		fmt.Println(newX, newY)
-		if newX >= 0 && newY >= 0 && parkTd[newX][newY] != "X" {
-			location[0], location[1] = newX, newY 	
-		}
 	}
-	
 	return location
+
 }
 
-func move(x, y int, order string) (int,int){
+func move(x, y int, order string, parkTd [][]string) (int,int){
     route := strings.Split(order, " ")
 	op := route[0]
 	n, _ := strconv.Atoi(route[1])
 
 	switch op {
 	case "N":
+		if x - n < 0 {
+			return x, y
+		}
+		for i:=x-1; i >= x-n ; i-- {
+			if parkTd[i][y] == "X"{
+				return x, y
+			}
+		}
 		x -= n
 	case "S" :
+		if x + n >= len(parkTd) {
+			return x, y
+		}
+		for i:=x+1; i <= x+n ; i++ {
+			if parkTd[i][y] == "X"{
+				return x, y
+			}
+		}
 		x += n
 	case "W" :
+		if y - n < 0 {
+			return x, y
+		}
+		for i:=y-1; i >= y-n ; i-- {
+			if parkTd[x][i] == "X"{
+				return x, y
+			}
+		}
 		y -= n
 	case "E" :
+		if y + n >= len(parkTd[0]) {
+			return x, y
+		}
+		for i:=y+1; i <= y+n ; i++ {
+			if parkTd[x][i] == "X"{
+				return x, y
+			}
+		}
 		y += n
 	}
-
 	return x, y
 }
