68dc00c205d6fd988b534bdd98891423370da64d
jaethewiederholen <herakjy0705@gmail.com>
2024-08-29 21:00:09 +0900

    Feat: full search

--- programmers/level2/sequence/solve.go
+++ programmers/level2/sequence/solve.go
@@ -2,37 +2,28 @@ package sequence
 
 import "math"
 
-
-func solution(sequence []int, k int) []int {
-    seqLen := int(math.Pow(10,6))
-    startIdx, endIdx :=int(math.Pow(10,6)) , 0
-    for i:= len(sequence)-1; i >= 0; i-- {
-        sum := sequence[i]
-        if sum == k {
-            if i < startIdx {
-                startIdx, endIdx = i, i
-                seqLen = 0
-            }
-        }
-        for j:= i-1; j>= 0; j-- {
-            sum += sequence[j]
-            if i-j > seqLen {
-                break
-            } else if sum > k { 
-                break
-            } else if sum == k { 
-                if i - j < seqLen {
-                    startIdx = j
-                    endIdx = i
-                } else if i - j == seqLen {
-                    if startIdx > j {
-                        startIdx = j
-                        endIdx = i
-                    }
-                }
-            }
-
-        }
-    }
-    return []int{startIdx, endIdx}
+func solution_fullSearch(sequence []int, k int) []int {
+	seqLen := int(math.Pow(10, 6))
+	startIdx, endIdx := int(math.Pow(10, 6)), 0
+	for i := 0; i <= len(sequence)-1; i++ {
+		sum := sequence[i]
+		if sum == k {
+			startIdx, endIdx = i, i
+			break
+		}
+		for j := i + 1; j <= len(sequence)-1; j++ {
+			sum += sequence[j]
+			if j-i > seqLen {
+				break
+			}
+			if sum == k {
+				if j-i < seqLen {
+					startIdx, endIdx = i, j
+					seqLen = j - i
+					break
+				}
+			}
+		}
+	}
+	return []int{startIdx, endIdx}
 }
