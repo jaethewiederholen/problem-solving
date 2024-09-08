402a80e19000f96cea47c6ccc3ddb29fb15d6aa9
jaethewiederholen <herakjy0705@gmail.com>
2024-09-08 21:31:46 +0900

    Feat: median of two arrs

--- playground/main.go
+++ playground/main.go
@@ -2,32 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func solution(nums []int, target int) []int {
-    type aInfo struct {
-        idx int
+func solution(nums1 []int, nums2 []int) float64 {
+    mergedArr := append(nums1, nums2...)
+    sort.Slice(mergedArr, func(i, j int) bool {
+        return mergedArr[i] < mergedArr[j]
+    })
+    if len(mergedArr)%2 == 0 {
+        //짝수개
+        return float64(mergedArr[len(mergedArr)/2-1] + mergedArr[len(mergedArr)/2]) / 2
+    } else {
+        return float64(mergedArr[len(mergedArr)/2])
     }
-    bDic := make(map[int]*aInfo)
-    
-    ans := make([]int, 2)
-   
-    for idx, num := range nums {
-        if bDic[num] != nil {
-            ans[0] = bDic[num].idx
-            ans[1] = idx
-            break
-        } else {
-            bDic[target-num] = &aInfo{idx: idx}
-        }
-
-    }
-    return ans
 }
 
 func main() {
     //var sequence [][]int = [][]int{{0,4}, {1,2}, {1,3}, {3,4}}
-    fmt.Println(solution([]int{3,2,4}, 6))
+    fmt.Println(solution([]int{1,2}, []int{3,4}))
 //     [[0, 4], [1, 2], [1, 3], [3, 4]] => 2
 //     [[0, 4], [0, 1], [2, 3]] => 2
 	
