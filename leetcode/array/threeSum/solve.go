09fa56244d35a97fb23c017165db420b3ee03880
jaethewiederholen <herakjy0705@gmail.com>
2024-09-11 20:26:30 +0900

    Feat: fix three sum

--- leetcode/array/threeSum/solve.go
+++ leetcode/array/threeSum/solve.go
@@ -1,7 +1,6 @@
-package main
+package threeSum
 
 import (
-	"fmt"
 	"sort"
 )
 
@@ -23,7 +22,6 @@ func solution(nums []int) [][]int {
 	sort.Slice(nums, func(i, j int) bool {
 		return nums[i] < nums[j]
 	})
-	fmt.Println(nums)
 	threeSumAns := make(map[[3]int]bool)
 
 	for i := 0; i < len(nums); i++ {
@@ -50,12 +48,4 @@ func solution(nums []int) [][]int {
 		ans = append(ans, []int{threeSum[0], threeSum[1], threeSum[2]})
 	}
 	return ans
-}
-
-func main() {
-	//var sequence [][]int = [][]int{{0,4}, {1,2}, {1,3}, {3,4}}
-	fmt.Println(solution([]int{-2, 0, 0, 2, 2}))
-	//     [[0, 4], [1, 2], [1, 3], [3, 4]] => 2
-	//     [[0, 4], [0, 1], [2, 3]] => 2
-
 }
