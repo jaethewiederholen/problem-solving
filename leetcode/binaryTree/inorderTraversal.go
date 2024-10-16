66ab12b6920cb94f36bc9c1fbb6938feae6c4f86
jaethewiederholen <herakjy0705@gmail.com>
2024-10-16 20:21:38 +0900

    Feat: inorder traversal

---
+++ leetcode/binaryTree/inorderTraversal.go
@@ -0,0 +1,22 @@
+package binaryTree
+
+type TreeNode struct {
+	Val int
+     Left *TreeNode
+     Right *TreeNode
+}
+
+func InorderTraversal(root *TreeNode) []int {
+    inorder := []int{}
+    var dfs func(node *TreeNode)
+    dfs = func(node *TreeNode){
+        if node == nil {
+            return
+        }
+        dfs(node.Left)
+        inorder = append(inorder, node.Val)
+        dfs(node.Right)
+    }
+    dfs(root)
+    return inorder
+}
